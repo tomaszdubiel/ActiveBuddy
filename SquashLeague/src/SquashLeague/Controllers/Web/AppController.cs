52299f0c72038a7fd528f126a12e124dad5416a3
tomaszdubiel <tomasz.dubiel@yahoo.com>
2016-10-23 13:52:04 +0100

    MVC 6 finished - works

--- SquashLeague/src/SquashLeague/Controllers/Web/AppController.cs
+++ SquashLeague/src/SquashLeague/Controllers/Web/AppController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SquashLeague.Services;
+using SquashLeague.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +11,46 @@ namespace SquashLeague.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if(model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We don't support AOL addresses");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From Squash", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
+            
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
